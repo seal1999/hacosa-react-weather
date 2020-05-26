e4629abc830a2a7130451589fabfe54eb223b758
Seungkwon Kim <teacher.seal@gmail.com>
2020-05-27 04:38:16 +0900

    App.js 컴포넌트 -> 함수형

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import { toggleMenu } from '../reducers/navigation';
 import { Switch, Route } from 'react-router-dom';
 import Aside from '../components/Aside';
@@ -11,38 +10,24 @@ import Add from '../components/Add';
 import Login from '../components/Login';
 import Details from '../components/Details';
 import NoMatch from '../components/NoMatch';
+import '../css/App.css';
 
-const Container = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  flex-flow: column;
-  transition: background 300ms linear;
-  background-image: linear-gradient(to top, #5ee7df 0%, #66a6ff 100%);
-`;
-
-class App extends Component {
-  render() {
-    const { showMenu, onToggleMenu, currentUser } = this.props;
-    return (
-      <div className="container">
-        <Container>
-          <Aside showMenu={showMenu} currentUser={currentUser} />
-          <Header onToggleMenu={onToggleMenu} />
-          <Switch>
-            <Route exact path="/hacosa-react-weather" component={Main} />
-            <Route path="/add" component={Add} />
-            <Route path="/login" component={Login} />
-            <Route path="/details" component={Details} />
-            <Route component={NoMatch} />
-          </Switch>
-          <Footer />
-        </Container>
-      </div>
-    );
-  }
-}
+const App = ({ showMenu, onToggleMenu }) => {
+  return (
+    <div className="container">
+      <Aside showMenu={showMenu} />
+      <Header onToggleMenu={onToggleMenu} />
+      <Switch>
+        <Route exact path="/hacosa-react-weather" component={Main} />
+        <Route path="/add" component={Add} />
+        <Route path="/login" component={Login} />
+        <Route path="/details" component={Details} />
+        <Route component={NoMatch} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   showMenu: state.navigation.showMenu,
